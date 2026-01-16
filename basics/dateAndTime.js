2d53f3588ab9cd515223e4654b9893b5f961eae4
kanishks201 <kanishks201@gmail.com>
2026-01-16 19:52:28 +0530

    Date and Time

---
+++ basics/dateAndTime.js
@@ -0,0 +1,19 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2026, 0, 11)
+// console.log(myCreatedDate.toLocaleDateString());
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
